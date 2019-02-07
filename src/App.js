d48e07789d8c1d0b06acf92167bec237acd8361d
owencyr <cyroc@dukes.jmu.edu>
2019-02-07 12:07:11 -0500

    added shell crud functions

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import List from './List';
 import { STORE } from './store';
 
 class App extends Component {
-  static defaultProps = STORE;
+  // static defaultProps = STORE;
+
+  state = {
+    STORE
+  }
+
+  deleteCard = (listid, cardindex) => {
+    this.setState({key:this.STORE.lists[listid-1].cardIds.filter(()=>{key,  ...rest}, ()=>{
+      return {...rest}
+    })
+  }
+
+  addRandomCard = () => {
+
+  }
 
   render() {
     let list = this.props.lists.map((list, index) => {
